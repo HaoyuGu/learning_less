284c44472820533e748344c2d0e61f727f466f7b
顾昊昱 <guhy3@chinatelecom.cn>
2022-09-06 17:30:09 +0800

    initial commit

---
+++ src/pages/Test/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { PageContainer } from '@ant-design/pro-components';
+
+const Test = () => {
+  return <PageContainer>我是test</PageContainer>;
+};
+
+export default Test;
